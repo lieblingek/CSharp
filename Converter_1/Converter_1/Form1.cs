6c0d33e362201ff83fbcda6b18920cc1d9d470fb
Vince Zsolt <l1@vincekolcsonzo.hu>
2020-08-05 22:44:24 +0200

    Adatbázis feltöltés 2/3-ad kész

--- Converter_1/Converter_1/Form1.cs
+++ Converter_1/Converter_1/Form1.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography.X509Certificates;
+using System.Globalization;
 
 namespace Converter_1
 {
@@ -20,17 +21,37 @@ namespace Converter_1
 			InitializeComponent();
 		}
 
-		public struct szamla
+		public struct ceg
 		{
-			public string szamlaszam;
+			public string cegnev;
+			public int kov_ceg;
+			public string adoszam;
+		}
+		public struct szamla_tetelek
+		{
+			public string bizonylatszam;
 			public string datum;
 			public float netto;
+			public string afa_szaz;
 			public float afa;
+			public float brutto;
+			public string cegnev;
+			public int kov_szamla;
+		}
+		public struct szamla_ossz
+		{
+			public string bizonylatszam;
+			public string datum;
 			public float brutto;
 		}
 
 		public string parsFile1(string textIn) {
-			szamla[,] main = new szamla[50,50];
+			ceg[] ceglista = new ceg[50];
+			szamla_tetelek[,] szamla_tetel = new szamla_tetelek[50,70];
+			szamla_ossz[,] szamla_osszegzo = new szamla_ossz[50,20];
+			int kov_ceg_sorsz = 0;
+			int ceg_most;
+			int kov_tetel_sorszam = 0;
 			string retstr = "Add text" + textIn;
 			// file ból tábala, [1] a fő tábla
 			string[] strarr = textIn.Split(new[] { "table" }, StringSplitOptions.None);
@@ -39,19 +60,19 @@ namespace Converter_1
 
 			treeView1.BeginUpdate();
 
-			treeView1.Nodes.Add("Cég 1");
-			treeView1.Nodes[0].Nodes.Add("Számla 1");
-			treeView1.Nodes[0].Nodes[0].Nodes.Add("Sor 1");
-			treeView1.Nodes[0].Nodes[0].Nodes.Add("Sor 2");
-			treeView1.Nodes[0].Nodes[0].Nodes.Add("Sor 3");
-			treeView1.Nodes[0].Nodes.Add("Számla 2");
-			treeView1.Nodes[0].Nodes[1].Nodes.Add("Sor 4");
-			treeView1.Nodes[0].Nodes[1].Nodes.Add("Sor 5");
-			treeView1.Nodes[0].Nodes[1].Nodes.Add("Sor 6");
-			treeView1.Nodes.Add("Data 1");
-			treeView1.Nodes.Add("Data 2");
-			treeView1.Nodes.Add("Data 3");
-			treeView1.Nodes.Add("Data 4");
+			treeView1.Nodes.Add("File 1 adatok");
+			//treeView1.Nodes[0].Nodes.Add("Számla 1");
+			//treeView1.Nodes[0].Nodes[0].Nodes.Add("Sor 1");
+			//treeView1.Nodes[0].Nodes[0].Nodes.Add("Sor 2");
+			//treeView1.Nodes[0].Nodes[0].Nodes.Add("Sor 3");
+			//treeView1.Nodes[0].Nodes.Add("Számla 2");
+			//treeView1.Nodes[0].Nodes[1].Nodes.Add("Sor 4");
+			//treeView1.Nodes[0].Nodes[1].Nodes.Add("Sor 5");
+			//treeView1.Nodes[0].Nodes[1].Nodes.Add("Sor 6");
+			//treeView1.Nodes.Add("Data 1");
+			//treeView1.Nodes.Add("Data 2");
+			//treeView1.Nodes.Add("Data 3");
+			//treeView1.Nodes.Add("Data 4");
 			List<string> line = new List<string>();
 			foreach (string st in strarr2)
 			{
@@ -82,10 +103,88 @@ namespace Converter_1
 						}
 					}
 					string[] adatb = line.ToArray();
-					treeView1.Nodes[3].Nodes.Add("++++++++++++ new 3 +++++++++++++");
 					string tests = "Adat0: " + adatb[0] + " Adat1: " + adatb[1] + " Adat2: " + adatb[2] + " Adat3: " + adatb[3] + " Adat4: " + adatb[4] + " Adat5: " + adatb[5];
 					tests += " Adat6: " + adatb[6] + " Adat7: " + adatb[7] + " Adat8: " + adatb[8];
-					treeView1.Nodes[3].Nodes.Add(tests);
+					if (kov_ceg_sorsz == 0)
+					{
+						ceglista[0].cegnev = adatb[2];
+						ceglista[0].adoszam = adatb[3];
+						ceglista[0].kov_ceg = 0;
+						szamla_tetel[0, 0].bizonylatszam = adatb[1];
+						szamla_tetel[0, 0].datum = adatb[0];
+						szamla_tetel[0, 0].netto = float.Parse(adatb[5], CultureInfo.InvariantCulture.NumberFormat);
+						szamla_tetel[0, 0].afa_szaz = adatb[6];
+						szamla_tetel[0, 0].afa = float.Parse(adatb[7], CultureInfo.InvariantCulture.NumberFormat);
+						szamla_tetel[0, 0].brutto = float.Parse(adatb[8], CultureInfo.InvariantCulture.NumberFormat);
+						szamla_tetel[0, 0].cegnev = adatb[2];
+						szamla_tetel[0, 0].kov_szamla = 0;
+						kov_ceg_sorsz++;
+					}
+					else 
+					{
+						ceg_most = 51;
+						for (int i = 0; i < kov_ceg_sorsz + 2; i++) 
+						{
+							if (ceglista[i].cegnev == adatb[2]) 
+							{
+								ceg_most = i;
+								i = 51;
+							}
+						}
+						if (ceg_most > 50)
+						{
+							ceglista[kov_ceg_sorsz].cegnev = adatb[2];
+							ceglista[kov_ceg_sorsz].adoszam = adatb[3];
+							ceglista[kov_ceg_sorsz].kov_ceg = 0;
+							ceglista[kov_ceg_sorsz - 1].kov_ceg = 1;
+							szamla_tetel[kov_ceg_sorsz, 0].bizonylatszam = adatb[1];
+							szamla_tetel[kov_ceg_sorsz, 0].datum = adatb[0];
+							szamla_tetel[kov_ceg_sorsz, 0].netto = float.Parse(adatb[5], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[kov_ceg_sorsz, 0].afa_szaz = adatb[6];
+							szamla_tetel[kov_ceg_sorsz, 0].afa = float.Parse(adatb[7], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[kov_ceg_sorsz, 0].brutto = float.Parse(adatb[8], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[kov_ceg_sorsz, 0].cegnev = adatb[2];
+							szamla_tetel[kov_ceg_sorsz, 0].kov_szamla = 0;
+							kov_ceg_sorsz++;
+						}
+						else
+						{
+							kov_tetel_sorszam = 0;
+							for (int i = 0; i < 50; i++) 
+							{
+								if (szamla_tetel[ceg_most, i].kov_szamla == 0) 
+								{
+									kov_tetel_sorszam = i + 1;
+									i = 51;
+								}
+							}
+							szamla_tetel[ceg_most, kov_tetel_sorszam].bizonylatszam = adatb[1];
+							szamla_tetel[ceg_most, kov_tetel_sorszam].datum = adatb[0];
+							szamla_tetel[ceg_most, kov_tetel_sorszam].netto = float.Parse(adatb[5], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[ceg_most, kov_tetel_sorszam].afa_szaz = adatb[6];
+							szamla_tetel[ceg_most, kov_tetel_sorszam].afa = float.Parse(adatb[7], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[ceg_most, kov_tetel_sorszam].brutto = float.Parse(adatb[8], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[ceg_most, kov_tetel_sorszam].cegnev = adatb[2];
+							szamla_tetel[ceg_most, kov_tetel_sorszam].kov_szamla = 0;
+							szamla_tetel[ceg_most, kov_tetel_sorszam - 1].kov_szamla = 1;
+						}
+					}
+				}
+			}
+			for (int i = 0; i < 51; i++) 
+			{
+				treeView1.Nodes[0].Nodes.Add(ceglista[i].cegnev + " + " + ceglista[i].adoszam + " + " + ceglista[i].kov_ceg + " ind: " + i);
+				for (int k = 0; k < 71; k++)
+				{
+					treeView1.Nodes[0].Nodes[i].Nodes.Add("Biz: " + szamla_tetel[i, k].bizonylatszam + " date: " + szamla_tetel[i, k].datum + " netto: " + szamla_tetel[i, k].netto + " i: " + i + " k: " + k);
+					if (szamla_tetel[i, k].kov_szamla == 0)
+					{
+						k = 72;
+					}
+				}
+				if (ceglista[i].kov_ceg == 0)
+				{
+					i = 52;
 				}
 			}
 			treeView1.EndUpdate();
