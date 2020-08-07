904e4353dfd50f706684ee52fbb0fe45c38ec12e
Vince Zsolt <l1@vincekolcsonzo.hu>
2020-08-08 01:07:33 +0200

    bug fix, kerekites, számla feltöltés

--- Converter_1/Converter_1/Form1.Designer.cs
+++ Converter_1/Converter_1/Form1.Designer.cs
@@ -29,8 +29,12 @@
 		private void InitializeComponent()
 		{
             this.load = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // load
@@ -41,38 +45,52 @@
             this.load.TabIndex = 0;
             this.load.Text = "Load File 1\r\nSzámlák";
             this.load.UseVisualStyleBackColor = true;
-            this.load.Click += new System.EventHandler(this.load_Click);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.richTextBox1.Location = new System.Drawing.Point(12, 67);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(466, 552);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
+            this.load.Click += new System.EventHandler(this.Load_Click);
             // 
             // treeView1
             // 
             this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.treeView1.Location = new System.Drawing.Point(484, 67);
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(492, 552);
+            this.treeView1.Size = new System.Drawing.Size(492, 575);
             this.treeView1.TabIndex = 3;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(12, 67);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Size = new System.Drawing.Size(975, 581);
+            this.splitContainer1.SplitterDistance = 497;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(861, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 49);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Load file 2\r\nÁNYK xml";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(999, 660);
-            this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.load);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
 		}
@@ -80,8 +98,9 @@
 		#endregion
 
 		private System.Windows.Forms.Button load;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Converter_1/Converter_1/Form1.cs
+++ Converter_1/Converter_1/Form1.cs
@@ -21,13 +21,13 @@ namespace Converter_1
 			InitializeComponent();
 		}
 
-		public struct ceg
+		public struct Ceg
 		{
 			public string cegnev;
 			public int kov_ceg;
 			public string adoszam;
 		}
-		public struct szamla_tetelek
+		public struct Szamla_tetelek
 		{
 			public string bizonylatszam;
 			public string datum;
@@ -38,47 +38,36 @@ namespace Converter_1
 			public string cegnev;
 			public int kov_szamla;
 		}
-		public struct szamla_ossz
+		public struct Szamla_ossz
 		{
 			public string bizonylatszam;
 			public string datum;
 			public float brutto;
 			public float afa;
+			public double brutto_kerek;
+			public double afa_kerek;
 			public int kov_szamla;
 		}
 
-		public string parsFile1(string textIn) {
+		public string ParsFile1(string textIn) {
 			int max_ceg_szam = 100;
 			int max_tetel_szam = 100;
 			int max_szama_szam = 30;
-			ceg[] ceglista = new ceg[max_ceg_szam];
-			szamla_tetelek[,] szamla_tetel = new szamla_tetelek[max_ceg_szam, max_tetel_szam];
-			szamla_ossz[,] szamla_osszegzo = new szamla_ossz[max_ceg_szam, max_szama_szam];
+			Ceg[] ceglista = new Ceg[max_ceg_szam];
+			Szamla_tetelek[,] szamla_tetel = new Szamla_tetelek[max_ceg_szam, max_tetel_szam];
+			Szamla_ossz[,] szamla_osszegzo = new Szamla_ossz[max_ceg_szam, max_szama_szam];
 			int kov_ceg_sorsz = 0;
 			int ceg_most;
-			int szamla_most;
-			int kov_tetel_sorszam = 0;
-			string retstr = "Add text" + textIn;
 			// file ból tábala, [1] a fő tábla
 			string[] strarr = textIn.Split(new[] { "table" }, StringSplitOptions.None);
 			// táblából különböző áfakulcsok
 			string[] strarr2 = strarr[1].Split(new[] { "</tr>" }, StringSplitOptions.None);
+			
 
 			treeView1.BeginUpdate();
 
 			treeView1.Nodes.Add("File 1 adatok");
-			//treeView1.Nodes[0].Nodes.Add("Számla 1");
-			//treeView1.Nodes[0].Nodes[0].Nodes.Add("Sor 1");
-			//treeView1.Nodes[0].Nodes[0].Nodes.Add("Sor 2");
-			//treeView1.Nodes[0].Nodes[0].Nodes.Add("Sor 3");
-			//treeView1.Nodes[0].Nodes.Add("Számla 2");
-			//treeView1.Nodes[0].Nodes[1].Nodes.Add("Sor 4");
-			//treeView1.Nodes[0].Nodes[1].Nodes.Add("Sor 5");
-			//treeView1.Nodes[0].Nodes[1].Nodes.Add("Sor 6");
-			//treeView1.Nodes.Add("Data 1");
-			//treeView1.Nodes.Add("Data 2");
-			//treeView1.Nodes.Add("Data 3");
-			//treeView1.Nodes.Add("Data 4");
+			treeView1.Nodes.Add("File 1 adatok 2");
 			List<string> line = new List<string>();
 			foreach (string st in strarr2)
 			{
@@ -105,14 +94,14 @@ namespace Converter_1
 						string[] strarr4 = st2.Split(new[] { "041A93\">" }, StringSplitOptions.None);
 						if (strarr4.Length > 1)
 						{
-							strarr4[1].Replace(",", ".");
 							line.Add(strarr4[1]);
 						}
 					}
 					string[] adatb = line.ToArray();
-					adatb[5].Replace(",", ".").Replace("0", "9");
-					adatb[7].Replace(",", ".").Replace("0", "9");
-					adatb[8].Replace(",", ".").Replace("0", "9");
+					Regex pattern = new Regex(",");
+					String test1 = adatb[5].Replace(',', '.');
+					String test2 = adatb[7].Replace(',', '.');
+					String test3 = adatb[8].Replace(',', '.');
 					string tests = "Adat0: " + adatb[0] + " Adat1: " + adatb[1] + " Adat2: " + adatb[2] + " Adat3: " + adatb[3] + " Adat4: " + adatb[4] + " Adat5: " + adatb[5];
 					tests += " Adat6: " + adatb[6] + " Adat7: " + adatb[7] + " Adat8: " + adatb[8];
 					if (kov_ceg_sorsz == 0)
@@ -122,10 +111,10 @@ namespace Converter_1
 						ceglista[0].kov_ceg = 0;
 						szamla_tetel[0, 0].bizonylatszam = adatb[1];
 						szamla_tetel[0, 0].datum = adatb[0];
-						szamla_tetel[0, 0].netto = float.Parse(adatb[5], CultureInfo.InvariantCulture);
+						szamla_tetel[0, 0].netto = float.Parse(test1, CultureInfo.InvariantCulture);
 						szamla_tetel[0, 0].afa_szaz = adatb[6];
-						szamla_tetel[0, 0].afa = float.Parse(adatb[7], CultureInfo.InvariantCulture.NumberFormat);
-						szamla_tetel[0, 0].brutto = float.Parse(adatb[8], CultureInfo.InvariantCulture.NumberFormat);
+						szamla_tetel[0, 0].afa = float.Parse(test2, CultureInfo.InvariantCulture.NumberFormat);
+						szamla_tetel[0, 0].brutto = float.Parse(test3, CultureInfo.InvariantCulture.NumberFormat);
 						szamla_tetel[0, 0].cegnev = adatb[2];
 						szamla_tetel[0, 0].kov_szamla = 0;
 						kov_ceg_sorsz++;
@@ -149,17 +138,17 @@ namespace Converter_1
 							ceglista[kov_ceg_sorsz - 1].kov_ceg = 1;
 							szamla_tetel[kov_ceg_sorsz, 0].bizonylatszam = adatb[1];
 							szamla_tetel[kov_ceg_sorsz, 0].datum = adatb[0];
-							szamla_tetel[kov_ceg_sorsz, 0].netto = float.Parse(adatb[5], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[kov_ceg_sorsz, 0].netto = float.Parse(test1, CultureInfo.InvariantCulture.NumberFormat);
 							szamla_tetel[kov_ceg_sorsz, 0].afa_szaz = adatb[6];
-							szamla_tetel[kov_ceg_sorsz, 0].afa = float.Parse(adatb[7], CultureInfo.InvariantCulture.NumberFormat);
-							szamla_tetel[kov_ceg_sorsz, 0].brutto = float.Parse(adatb[8], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[kov_ceg_sorsz, 0].afa = float.Parse(test2, CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[kov_ceg_sorsz, 0].brutto = float.Parse(test3, CultureInfo.InvariantCulture.NumberFormat);
 							szamla_tetel[kov_ceg_sorsz, 0].cegnev = adatb[2];
 							szamla_tetel[kov_ceg_sorsz, 0].kov_szamla = 0;
 							kov_ceg_sorsz++;
 						}
 						else
 						{
-							kov_tetel_sorszam = 0;
+							int kov_tetel_sorszam = 0;
 							for (int i = 0; i < max_ceg_szam; i++) 
 							{
 								if (szamla_tetel[ceg_most, i].kov_szamla == 0) 
@@ -170,10 +159,10 @@ namespace Converter_1
 							}
 							szamla_tetel[ceg_most, kov_tetel_sorszam].bizonylatszam = adatb[1];
 							szamla_tetel[ceg_most, kov_tetel_sorszam].datum = adatb[0];
-							szamla_tetel[ceg_most, kov_tetel_sorszam].netto = float.Parse(adatb[5], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[ceg_most, kov_tetel_sorszam].netto = float.Parse(test1, CultureInfo.InvariantCulture.NumberFormat);
 							szamla_tetel[ceg_most, kov_tetel_sorszam].afa_szaz = adatb[6];
-							szamla_tetel[ceg_most, kov_tetel_sorszam].afa = float.Parse(adatb[7], CultureInfo.InvariantCulture.NumberFormat);
-							szamla_tetel[ceg_most, kov_tetel_sorszam].brutto = float.Parse(adatb[8], CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[ceg_most, kov_tetel_sorszam].afa = float.Parse(test2, CultureInfo.InvariantCulture.NumberFormat);
+							szamla_tetel[ceg_most, kov_tetel_sorszam].brutto = float.Parse(test3, CultureInfo.InvariantCulture.NumberFormat);
 							szamla_tetel[ceg_most, kov_tetel_sorszam].cegnev = adatb[2];
 							szamla_tetel[ceg_most, kov_tetel_sorszam].kov_szamla = 0;
 							szamla_tetel[ceg_most, kov_tetel_sorszam - 1].kov_szamla = 1;
@@ -227,28 +216,40 @@ namespace Converter_1
 					i = max_ceg_szam;
 				}
 			}
+			for (int i = 0; i < max_ceg_szam; i++)
+			{
+				treeView1.Nodes[1].Nodes.Add(ceglista[i].cegnev + " + " + ceglista[i].adoszam + " + " + ceglista[i].kov_ceg + " ind: " + i);
+				for (int k = 0; k < max_tetel_szam; k++)
+				{
+					szamla_osszegzo[i, k].brutto_kerek = Math.Round(szamla_osszegzo[i, k].brutto / 1000);
+					szamla_osszegzo[i, k].afa_kerek = Math.Round(szamla_osszegzo[i, k].afa / 1000);
+					treeView1.Nodes[1].Nodes[i].Nodes.Add("Biz: " + szamla_osszegzo[i, k].bizonylatszam + " date: " + szamla_osszegzo[i, k].datum + " afa: " + szamla_osszegzo[i, k].afa + " brutto: " + szamla_osszegzo[i, k].brutto + " brutto kerek: " + szamla_osszegzo[i, k].brutto_kerek + " afa kerek: " + szamla_osszegzo[i, k].afa_kerek);
+					if (szamla_osszegzo[i, k].kov_szamla == 0)
+					{
+						k = max_tetel_szam;
+					}
+				}
+				if (ceglista[i].kov_ceg == 0)
+				{
+					i = max_ceg_szam;
+				}
+			}
 			treeView1.EndUpdate();
-
-			//string retstr = "Almakorte";
-			//string retstr2 = strarr.Reverse.;
 			return strarr2[0];
-
-
 		}
-		private void load_Click(object sender, EventArgs e)
+		private void Load_Click(object sender, EventArgs e)
 		{
-			Stream file1_stream;
 			OpenFileDialog ofd = new OpenFileDialog();
 			ofd.Title = "Számlák file megnyitása";
 			ofd.Filter = "HTML file|*.htm|All file|*.*";
 			if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK) 
 			{
-				if ((file1_stream = ofd.OpenFile()) != null) {
+				if ((ofd.OpenFile()) != null) {
 					string strFileName = ofd.FileName;
 					string file1text = File.ReadAllText(strFileName, Encoding.Default);
 					// Comment 1
-					richTextBox1.Text = parsFile1(file1text);
-					//richTextBox1.Text = parsFile1(strFileName);
+					ParsFile1(file1text);
+					//richTextBox1.Text = ParsFile1(file1text);
 				}
 			}
 		}
